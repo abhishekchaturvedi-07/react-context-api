ba51d3fc1151226183fd18144d8856040dc8dad0
Abhishek <abhishekchaturvedi789@gmail.com>
2021-04-27 01:52:40 +0530

    useContext() example in comp2

--- src/Comp2.jsx
+++ src/Comp2.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+// From useContext
+
+import React, {useContext} from 'react'
 import Comp3 from './Comp3';
+import { FirstName, LastName } from './App';
 function Comp2() {
+    const fName = useContext(FirstName)
+    const lName = useContext(LastName)
     return (
         <div>
+            Comp 2 Data - useContext() - My Name is {fName} {lName}
             <Comp3 />
         </div>
     )
